dee426b3c88225c213618239a413077fe9088317
danialessi <daniele.alessi@hotmail.it>
2021-06-08 18:28:13 +0200

    aggiunti fumetti al main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('main_content')
-    <h1>main</h1>
+    <section class="comics-section black-background">
+        <div class="container">
+            {{-- title  --}}
+            <div class="section-title">
+                <h3>CURRENT SERIES</h3>
+            </div>
+            {{-- end title  --}}
+    
+            {{-- comics  --}}
+            <div class="comics-container">
+                @foreach ($comics as $comic)
+                    <div class="single-comic">
+                        <img src="{{ $comic['thumb'] }}" alt="">
+                        <h5>{{ $comic['series'] }}</h5>
+                    </div>
+                @endforeach
+                
+            </div>
+            {{-- end comics  --}}
+        
+        </div>
+    </section>
+    
+    
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,48 @@
 <header>
-    header
+    <div class="top-header">
+        <div class="container">
+            <div class="top-text-container">
+                <span>dc power℠ visa®</span>
+                <span>additional dc sites</span>
+            </div>
+        </div>
+    </div>
+
+    {{-- navbar  --}}
+    <div class="container">
+        <div class="nav-logo-container">
+            
+            <div class="logo">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="dc-logo">
+            </div>
+
+            <nav>
+                <ul class="nav-list">
+                    <li><a href="">characters</a></li>
+                    <li><a href="">comics</a></li>
+                    <li><a href="">movies</a></li>
+                    <li><a href="">tv</a></li>
+                    <li><a href="">games</a></li>
+                    <li><a href="">collectibles</a></li>
+                    <li><a href="">videos</a></li>
+                    <li><a href="">fans</a></li>
+                    <li><a href="">news</a></li>
+                    <li><a href="">shop</a></li>
+                </ul>
+            </nav>
+
+            <div class="search-bar">
+                <input type="text" placeholder="Search">
+            </div>
+        </div>
+        
+    </div>
+    {{-- end navbar  --}}
+
+     {{-- jumbotron  --}}
+     <section>
+        <div class="jumbotron"></div>
+    </section>
+    {{-- end jumbotron --}}
+
 </header>
