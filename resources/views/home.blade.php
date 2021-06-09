fcf2b5def9660787d4a630eca65c249602d9cd1e
danialessi <daniele.alessi@hotmail.it>
2021-06-09 13:02:12 +0200

    aggiunti dettagli al main della home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,14 +13,54 @@
             <div class="comics-container">
                 @foreach ($comics as $comic)
                     <div class="single-comic">
-                        <img src="{{ $comic['thumb'] }}" alt="">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
                         <h5>{{ $comic['series'] }}</h5>
                     </div>
                 @endforeach
-                
             </div>
             {{-- end comics  --}}
-        
+
+            {{-- button more  --}}
+            <div class="button-container">
+                <div class="load-more">
+                    load more
+                </div>
+            </div>
+            
+            {{-- end button more  --}}
+        </div>
+    </section>
+
+    <section class="services-list">
+        <div class="container">
+            <div class="services-images-container">
+                
+                <div class="image-container">
+                    <img src="{{ asset('img/buy-comics-digital-comics.png')}}" alt="digital-comics">
+                    <span>Digital comics</span>
+                </div>
+
+                <div class="image-container">
+                    <img src="{{ asset('img/buy-comics-merchandise.png')}}" alt="digital-comics">
+                    <span>DC Merchandise</span>
+                </div>
+
+                <div class="image-container">
+                    <img src="{{ asset('img/buy-comics-subscriptions.png')}}" alt="digital-comics">
+                    <span>Subscription</span>
+                </div>
+
+                <div class="image-container">
+                    <img src="{{ asset('img/buy-comics-shop-locator.png')}}" alt="digital-comics">
+                    <span>Comic Shop Locator</span>
+                </div>
+
+                <div class="image-container">
+                    <img src="{{ asset('img/buy-dc-power-visa.svg')}}" alt="digital-comics">
+                    <span>DC Power Visa</span>
+                </div>
+                
+            </div>
         </div>
     </section>
     
